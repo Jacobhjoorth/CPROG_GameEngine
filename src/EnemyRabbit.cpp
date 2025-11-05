e362e30a92b365733c89c5bd083879994ae06190
Jacob Hjorth <113351275+Jacobhjoorth@users.noreply.github.com>
2025-11-05 08:32:22 +0100

    Update EnemyRabbit.cpp

--- src/EnemyRabbit.cpp
+++ src/EnemyRabbit.cpp
@@ -6,33 +6,28 @@
 
 using namespace GameEngine;
 
-// Constructor: Initializes the rabbit's position, size, texture, and creation time
 EnemyRabbit::EnemyRabbit(int x, int y, int w, int h) : Sprite(x, y, w, h)
 {
     rabbitTexture = IMG_LoadTexture(sys.get_ren(), (constants::gResPath + "images/EnemyRabbit.png").c_str());
     setCreationTime(SDL_GetTicks()); // Store current time to track lifetime
 }
 
-// Static factory method to create a rabbit instance
 EnemyRabbit *EnemyRabbit::getInstance(int x, int y, int w, int h)
 {
     return new EnemyRabbit(x, y, w, h);
 }
 
-// Destructor: Release texture memory
 EnemyRabbit::~EnemyRabbit()
 {
     SDL_DestroyTexture(rabbitTexture);
 }
 
-// Renders the rabbit to the screen
 void EnemyRabbit::draw() const
 {
     const SDL_Rect &rect = getRect();
     SDL_RenderCopy(sys.get_ren(), rabbitTexture, NULL, &rect);
 }
 
-// Called every game tick to handle logic
 void EnemyRabbit::tick()
 {
     autoDelete(); // Check if the rabbit should despawn
@@ -46,8 +41,8 @@ void EnemyRabbit::autoDelete()
     // If rabbit has existed for 3 seconds
     if (SDL_GetTicks() - getCreationTime() >= 3000)
     {
-        spritesToRemove.push_back(this);     // Mark self for removal
-        ge.reducePlayerHealth();             // Penalize player health
+        spritesToRemove.push_back(this);    
+        ge.reducePlayerHealth();             
     }
 
     for (Sprite *c : spritesToRemove)
@@ -56,25 +51,21 @@ void EnemyRabbit::autoDelete()
     }
 }
 
-// Records the rabbit's creation time
 void EnemyRabbit::setCreationTime(Uint32 time)
 {
     creationTime = time;
 }
 
-// Retrieves how long the rabbit has existed
 Uint32 EnemyRabbit::getCreationTime() const
 {
     return creationTime;
 }
 
-// Returns a string representing the type
 std::string EnemyRabbit::getType() const
 {
     return "EnemyRabbit";
 }
 
-// Mark this object for deletion on game restart
 bool EnemyRabbit::deleteAtRestart() const
 {
     return true;
