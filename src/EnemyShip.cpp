5817b80d2ee042d1739901e3532d5153ba88fd3d
Jacob Hjorth <113351275+Jacobhjoorth@users.noreply.github.com>
2025-11-05 08:33:38 +0100

    Update EnemyShip.cpp

--- src/EnemyShip.cpp
+++ src/EnemyShip.cpp
@@ -6,37 +6,32 @@
 
 using namespace GameEngine;
 
-// Constructor: Set position, dimensions, and load texture for the enemy ship
 EnemyShip::EnemyShip(int x, int y, int w, int h) : Sprite(x, y, w, h)
 {
     shipTexture = IMG_LoadTexture(sys.get_ren(), (constants::gResPath + "images/Enemy.png").c_str());
 }
 
-// Static factory method to create an EnemyShip
 EnemyShip *EnemyShip::getInstance(int x, int y, int w, int h)
 {
     return new EnemyShip(x, y, w, h);
 }
 
-// Destructor: Clean up texture memory
 EnemyShip::~EnemyShip()
 {
     SDL_DestroyTexture(shipTexture);
 }
 
-// Renders the enemy ship on screen
 void EnemyShip::draw() const
 {
     const SDL_Rect &rect = getRect();
     SDL_RenderCopy(sys.get_ren(), shipTexture, NULL, &rect);
 }
 
-// Called every frame: moves ship and checks if it escapes the screen
 void EnemyShip::tick()
 {
-    moveY(); // Move downward
+    moveY(); 
 
-    // If ship moves past bottom of screen, penalize player and remove ship
+    // If ship moves past bottom of screen, reduce player health and remove ship
     if (getRect().y >= sys.SCREEN_HEIGHT)
     {
         ge.reducePlayerHealth();
@@ -50,13 +45,11 @@ void EnemyShip::moveY()
     rect.y += 2;
 }
 
-// Returns string for identifying the type
 std::string EnemyShip::getType() const
 {
     return "EnemyShip";
 }
 
-// Marks object for deletion during a game restart
 bool EnemyShip::deleteAtRestart() const
 {
     return true;
