4fef880bcfb885546956faa1f7ab2a92fea9a610
Jacob Hjorth <113351275+Jacobhjoorth@users.noreply.github.com>
2025-11-05 08:37:16 +0100

    Update HealthHandler.cpp

--- src/HealthHandler.cpp
+++ src/HealthHandler.cpp
@@ -3,18 +3,15 @@
 
 using namespace GameEngine;
 
-// Constructor: Initializes player health and number of enemies to kill
 HealthHandler::HealthHandler() : Sprite(100, 100, 100, 100)
 {
-    // Set total health points
     ge.setTotalHealth(3);
 
-    // Set current health
     ge.setCurrentHealth(3);
 
-    // UI text for number of enemies to kill
     // - STARSHIP -
     ge.setEnemiesToKillTxt("Ships to destroy: ");
+    
     // - RABBITHAMMER -
     // ge.setEnemiesToKillTxt("Rabbits to hit: ");
 
@@ -22,7 +19,6 @@ HealthHandler::HealthHandler() : Sprite(100, 100, 100, 100)
     ge.setEnemiesToKill(20);
 }
 
-// Factory method to create a HealthHandler instance
 HealthHandler *HealthHandler::getInstance()
 {
     return new HealthHandler();
@@ -30,15 +26,12 @@ HealthHandler *HealthHandler::getInstance()
 
 HealthHandler::~HealthHandler() {}
 
-// This component does not render anything
 void HealthHandler::draw() const {}
 
-// This component has no tick behavior
 void HealthHandler::tick()
 {
 }
 
-// Should be removed when the game resets
 bool HealthHandler::deleteAtRestart() const
 {
     return true;
